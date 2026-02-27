fe519b6a0c55edc16732c62f8fb0612eccc0a507
Muhammad Haris <harrylife581@gmail.com>
2026-02-27 19:11:27 +0500

    g

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,5 +1,6 @@
 // Api controller function to manage clerk user with database
 // http://localhost:4000/api/user/webhooks
 const clerkWebhooks=async(req,res)=>{
-    
+    //create a svix instance
+    const webhook=new webhook
 }
