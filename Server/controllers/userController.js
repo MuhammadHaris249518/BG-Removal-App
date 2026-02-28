1ab3a83f74477e594fc3b23b6f1147287ef15f45
Muhammad Haris <harrylife581@gmail.com>
2026-02-28 12:52:14 +0500

    g

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -8,13 +8,13 @@ const clerkWebhooks=async(req,res)=>{
 await webhook.verify(JSON.stringify(req.body),{
     "svix-id":req.headers["svix-id"],
     "svix-timestamp":req.headers["svix-timestamp"],
-    "svix-signature": req.headers["svix-signa"]
+    "svix-signature": req.headers["svix-signature"]
 })
 const{data,type}=req.body
 switch (type){
     case "user.created":{
         const userData={
-            clerkI:data.id,
+            clerkId:data.id,
             email:data.email_addresses[0].email_address,
             firstName:data.first_name,
             lastName:data.last_name,
@@ -33,7 +33,7 @@ switch (type){
             lastName:data.last_name,
             photo:data.image_url
         }
-        await userModel.findOneANDUpdate({clerkId:data.id},userData)
+        await userModel.findOneAndUpdate({clerkId:data.id},userData)
         res.json({})
         break;
     }
@@ -48,7 +48,7 @@ switch (type){
 
 catch(error){
     console.log(error.message)
-    res.JSON({success:false,message:error.message})
+    res.json({success:false,message:error.message})
 
 }
 }
